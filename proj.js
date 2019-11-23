9aea4a16ca724d9995f69fa08bb7777c5dfddc97
Jerry <j.ross@centre.edu>
2019-11-23 10:27:04 -0500

    ok

--- proj.js
+++ proj.js
@@ -1,1 +1,82 @@
+var datapromise = d3.csv("master%data%sheet.csv")
+
+datapromise.then(
+function (alldata)
+{
+    console.log((alldata), "coolbeans")
+},
+
+ function(err)
+    {
+        console.log("nope", err)
+    }
+)
+
+
+var screen = {width: 600, height: 500}
+var margins = {top: 10, right: 50, bottom: 50, left:50}
+
+var svg = d3.select("svg")
+
+var setup = function (array2D)
+{
+    
+    d3.select("svg")
+        .attr("width", screen.width)
+        .attr("height", screen.height)
+        .append("g")
+        .attr("id", "graph")
+        .attr("transform", "translate(" + margins.left + ","+margins.top+")");
+    
+    var width= screen.width - margins.left-margins.right;
+    var height = screen.height - margins.top - margins.bottom;
+
+    
+    var xScale = d3.scaleLinear()
+        .domain([1983,2018])
+        .range([0,width])
+    
+    var yScale = d3.scaleLinear()
+        .domain([0,100])
+        .range([height, 0])
+    
+    var zScale = d3.scaleLinear()
+        .domain([7500,41000])
+        .range([height, 0])
+    
+    var aScale = d3.scaleLinear()
+        .domain([10,35])
+        .range([height,0])
+    
+    var xAxis = d3.axisBottom(xScale)
+    var yAxis = d3.axisLeft(yScale)
+    
+    
+    d3.select("svg")
+        .append("g")
+        .classed("axis", true)
+    
+    d3.select(".axis")
+        .append("g")
+        .attr("id", "xAxis")
+        .attr("transform", "translate(" + margins.left + "," + (margins.top+height) + ")")
+        .call(xAxis)
+    
+      d3.select(".axis")
+        .append("g")
+        .attr("id", "yAxis")
+        .attr("transform", "translate(25," + margins.top + ")")
+        .call(yAxis)
+
+}
+
+
+
+
+
+
+
+
+
+
 
